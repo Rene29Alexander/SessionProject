e07e2cf43478f660954bcca85fb507fc8ae79ded
Rene29Alexander <rene29alexander@gmail.com>
2021-09-04 16:47:51 -0600

    Agregar archivos de proyecto.

---
+++ SessionProject/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace SessionProject
+{
+    class Program
+    {
+        
+        static Session session = new Session();
+        static Boolean isAdmin = false;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INICIO DE SESSION");
+            login();
+
+        }
+
+
+        static void menus()
+        {
+            String option = Console.ReadLine();
+            option = option.ToLower();
+            String fileName = "";
+            String text = "";
+            Boolean exit = false;
+            if (isAdmin)
+            {
+                switch (option)
+                {
+                    case "a":
+                        Console.WriteLine("Agregar crendenciales.");
+                        Console.WriteLine("Ingrese el usuario: ");
+                        String username = Console.ReadLine();
+                        Console.WriteLine("Ingrese la contraseña: ");
+                        String paasword = Console.ReadLine();
+                        session.addCredential(username, paasword);
+                        break;
+                    case "b":
+                        Console.WriteLine("Crear archivo");
+                        Console.WriteLine("Nombre del archivo? ");
+                        fileName = Console.ReadLine();
+                        Console.WriteLine("Ingrese el texto del archivo");
+                        text = Console.ReadLine();
+                        session.newFile(fileName);
+                        session.addTextFile(text, fileName);
+                        break;
+                    case "c":
+                        Console.WriteLine("Borrar archivo");
+                        Console.WriteLine("Nombre del archivo? ");
+                        fileName = Console.ReadLine();
+                        session.deleteFile(fileName);
+                        break;
+                    case "d":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion ingresada incorrecta");
+
+                        break;
+
+                }
+                if (!exit)
+                {
+                    printMenuAdmin();
+                    menus();
+                }
+            }
+            else
+            {
+                switch (option)
+                {
+                    case "a":
+                        Console.WriteLine("NOMBRE DE ARCHIVOS .txt");
+                        Console.WriteLine("ReneAlexanderAraujoSotoSMIS060720.txt");
+                        Console.WriteLine("JonathanEliaGamezLarinSMIS017821,txt");
+                        Console.WriteLine("PlacidoIsmaelLunaArguetaSMIS071121");
+                        Console.WriteLine("AlexisManuelCalixMagañaSMIS021821");
+                        Console.WriteLine("Introduce el nombre del archivo que quiereas leer incluyendo el txt al final.");
+                        Console.WriteLine("Nombre del archivo: ");
+                        fileName = Console.ReadLine();
+                        text = session.readFile(fileName);
+                        Console.WriteLine(text);
+                        break;
+                    case "b":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion ingresada incorrecta");
+
+                        break;
+
+                }
+                if (!exit)
+                {
+                    printMenuOther();
+                    menus();
+                }
+            }
+        }
+
+        static void login()
+        {
+
+            
+           var contador = 1;
+            Console.WriteLine("Ingrese usuario?");
+            String username = Console.ReadLine();
+            Console.WriteLine("Ingrese contraseña?");
+            String password = Console.ReadLine();
+            if (session.login(username, password))
+            {
+                if (username.Equals("admin"))
+                {
+                    isAdmin = true;
+                    printMenuAdmin();
+                }
+                else
+                {
+                    isAdmin = false;
+                    printMenuOther();
+                }
+
+                menus();
+            }
+            else
+            {
+                Console.WriteLine("Usuario o contraseña incorrectos");
+                contador = contador + 1;
+                login();
+            }
+
+        }
+
+        static void printMenuAdmin()
+        {
+            Console.WriteLine("--------------- MENU ADMINISTRADOR--------------");
+            Console.WriteLine("\ta. Registrar nuevo usuario");
+            Console.WriteLine("\tb. Crear archivo");
+            Console.WriteLine("\tc. Borrar archivo");
+            Console.WriteLine("\td. Salir:");
+
+        }
+
+        static void printMenuOther()
+        {
+            Console.WriteLine("--------------- MENU ADMINISTRADOR--------------");
+            Console.WriteLine("\ta. Leer Archivo");
+            Console.WriteLine("\tb. Salir:");
+
+        }
+    }
+}
---
+++ SessionProject/Session.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SessionProject
+{
+    class FileManager
+    {
+        String pathDirectory = "";
+        public FileManager()
+        {
+
+            DirectoryInfo directoryInfo = System.IO.Directory.CreateDirectory(".\\store");
+            pathDirectory = directoryInfo.FullName;
+
+        }
+
+        public String readFile(String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            string text = System.IO.File.ReadAllText(fileName);
+            return text;
+        }
+
+        public String[] getLines(String file)
+        {
+            return this.readFile(file).Split('\n');
+        }
+
+        public async void writeFile(String text, String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            using StreamWriter fileStream = new StreamWriter(fileName, append: true);
+            await fileStream.WriteLineAsync(text);
+            fileStream.Close();
+        }
+
+
+        public void createFile(String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            System.IO.File.Create(fileName).Close();
+
+
+        }
+
+        public void deleteFile(String fileName)
+        {
+
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            System.IO.File.Delete(fileName);
+
+        }
+
+        public Boolean existsFile(String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+
+            return System.IO.File.Exists(fileName);
+
+        }
+
+
+
+        public async void deleteLine(int line, String fileName)
+        {
+            fileName = System.IO.Path.Combine(pathDirectory, fileName);
+            string text = this.readFile(fileName);
+            string[] lines = text.Split('\n');
+
+
+            await File.WriteAllLinesAsync(fileName, lines);
+        }
+    }
+
+    class Session
+    {
+
+        String fileStoreCredentials = "storecredentials.txt";
+        FileManager fileManager = null;
+        const string passwordAdmin = "123";
+        public Session()
+        {
+            this.fileManager = new FileManager();
+            if (!fileManager.existsFile(fileStoreCredentials))
+            {
+                fileManager.createFile(fileStoreCredentials);
+                fileManager.writeFile("admin:123", fileStoreCredentials);
+            }
+        }
+
+        public Boolean login(String username, String password)
+        {
+            Boolean loginSuccess = false;
+
+            if (username.Equals("admin") && password.Equals(passwordAdmin))
+                return true;
+
+            string[] lines = this.fileManager.getLines(this.fileStoreCredentials);
+            for (int index = 0; index < lines.Length; index++)
+            {
+
+                string[] credentials = lines[index].Split(':');
+
+                if (credentials.Length > 1)
+                {
+                    if (loginSuccess)
+                        break;
+
+                    if (credentials[0].Equals(username) && credentials[1].Replace("\r", "").Equals(password))
+                    {
+
+                        loginSuccess = true;
+                    }
+                    else
+                    {
+
+                        loginSuccess = false;
+                    }
+
+                }
+
+
+
+            }
+            return loginSuccess;
+        }
+
+        public void addCredential(String username, String password)
+        {
+            fileManager.writeFile(username + ":" + password, fileStoreCredentials);
+        }
+
+
+        public void addTextFile(String text, String fileName)
+        {
+
+            fileManager.writeFile(text, fileName);
+        }
+
+
+        public void deleteFile(String fileName)
+        {
+            fileManager.deleteFile(fileName);
+        }
+
+        public void newFile(String fileName)
+        {
+            fileManager.createFile(fileName);
+        }
+
+        public String readFile(String fileName)
+        {
+            return fileManager.readFile(fileName);
+        }
+
+
+    }
+}
